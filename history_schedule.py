83b2be7271b6273939df9fe7e8e8cf5203c5650b
datarocksAmy <clthf@mail.umkc.edu>
2018-03-31 21:54:06 -0500

    add more functions and fix logic issues

--- history_schedule.py
+++ history_schedule.py
@@ -114,6 +114,7 @@ def conflict_raid(finalHistT):
 def History_generator(dataItemL, numTransaction, numOperation):
     history = {}
     chainTransactions = []
+    countNumT = []
 
     for numT in range(1, numTransaction+1):
         transaction = JSON_Obj_generator(numOperation, numT, dataItemL)
@@ -130,14 +131,87 @@ def History_generator(dataItemL, numTransaction, numOperation):
     finalHistoryT = relabel(historyT)
 
     # Put in commit and abort for each transaction
-    countNumT = 1
-    while countNumT <= numTransaction:
-        for numT_count in range(1, len(finalHistoryT)):
-            if finalHistoryT[-numT_count]['Transaction'] == countNumT:
-                finalHistoryT[-numT_count]['Operation'] = operation_generator("ac")
-                countNumT += 1
+    for numT_count in range(1, len(finalHistoryT)+1):
+        if finalHistoryT[-numT_count]['Transaction'] not in countNumT:
+            countNumT.append(finalHistoryT[-numT_count]['Transaction'])
+            finalHistoryT[-numT_count]['Operation'] = operation_generator("ac")
+
+
+    # Label IsHistroy to True
+    for flipIdx in range(0, len(finalHistoryT)):
+        finalHistoryT[flipIdx]['IsHistory'] = "True"
 
     return finalHistoryT
+
+
+# Determine Serializability
+def serializability_pat(finalHistT):
+    conflict_Case = {
+                1: ["r", "w"],
+                2: ["w", "w"],
+                3: ["w", "r"]
+            }
+    conflict_Order = {1: [], 2: [], 3: []}
+
+    # Different Transaction, Same Data Item, One of them is write
+    for idx in range(1, len(finalHistT)+1):
+        for searchIdx in range(2, len(finalHistT)+1):
+            tempOp = []
+            if (finalHistT[idx-1]['Transaction'] != finalHistT[searchIdx-1]['Transaction']) and \
+                    (finalHistT[idx-1]['Data Item'] == finalHistT[searchIdx-1]['Data Item']):
+                        tempOp.append(finalHistT[idx-1]['Operation'])
+                        tempOp.append(finalHistT[searchIdx-1]['Operation'])
+                        temp_T_order = [finalHistT[idx-1]['Transaction'], finalHistT[searchIdx-1]['Transaction']]
+
+                        for caseNum in range(1, 4):
+                            if (tempOp == conflict_Case.get(caseNum)) and (temp_T_order not in conflict_Order[caseNum]):
+                                conflict_Order[caseNum].append(temp_T_order)
+
+    if (len(conflict_Order[1]) == len(conflict_Order[2]) and len(conflict_Order[2]) == len(conflict_Order[3]) and len(conflict_Order[1]) == len(conflict_Order[3])) and \
+            ((conflict_Order[1] == conflict_Order[2]) and (conflict_Order[2] == conflict_Order[3]) and (conflict_Order[1] == conflict_Order[3])):
+        return True
+    else:
+        return False
+
+# Get the Transaction order for abort and commit
+def AC_order(finalHistT):
+    ac_order_list = []
+    for idx in range(0, len(finalHistT)):
+        if (finalHistT[idx]['Operation'] == 'a' or finalHistT[idx]['Operation'] == 'c') and \
+                (finalHistT[idx]['Transaction'] not in ac_order_list):
+                    ac_order_list.append(finalHistT[idx]['Transaction'])
+
+    return ac_order_list
+
+
+# Determine RC or not
+def RC_mate(finalHistT):
+    abort_commit_dataI = []
+    for pivot in range(0, len(finalHistT)):
+        for idx in range(1, len(finalHistT)):
+            # Find different Transaction
+            if finalHistT[pivot]['Transaction'] != finalHistT[idx]['Transaction']:
+                # Find operation working on the same data item
+                if (finalHistT[pivot]['Data Item'] == finalHistT[idx]['Data Item']) and \
+                                finalHistT[pivot]['Operation'] == 'w' and \
+                                finalHistT[pivot]['Transaction'] not in abort_commit_dataI:
+                    abort_commit_dataI.append(finalHistT[pivot]['Transaction'])
+
+                    # Check if it's 'read from' or not
+                    if (finalHistT[idx]['Operation'] == 'r' or finalHistT[idx]['Operation'] == 'w') and \
+                            (finalHistT[pivot]['Transaction'] not in abort_commit_dataI):
+                            abort_commit_dataI.append(finalHistT[idx]['Transaction'])
+
+            elif finalHistT[pivot]['Transaction'] not in abort_commit_dataI:
+                abort_commit_dataI.append(finalHistT[pivot]['Transaction'])
+
+    # RC : abort and commit order = operation on the same data item transaction order
+    ###################################  Not finished with different RC cases just yet
+    if abort_commit_dataI == AC_order(finalHistT):
+        return "RC"
+    else:
+        return "NRC"
+
 
 
 # Prompt user input for num of Data Items & num of Transactions & Max Num of Operation
@@ -152,6 +226,7 @@ dataItemList = data_item_generator(numDataItem)
 # Generate History
 historyyy = History_generator(dataItemList, numTransaction, numOperation)
 
+
 # Check if there's at least one conflict in the history or not
 # If not, generate a history again
 while conflict_raid(historyyy):
@@ -163,3 +238,10 @@ with open("HistoryJSON.txt", 'w') as JSONfile:
     outputJSON = json.dumps(historyyy, indent=4)
     JSONfile.write(outputJSON)
 
+# Serializable or not
+serializability = serializability_pat(historyyy)
+print(serializability)
+
+
+test = RC_mate(historyyy)
+print(test)
